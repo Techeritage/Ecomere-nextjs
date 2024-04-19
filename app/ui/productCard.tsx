ab210dce07c2b59636b0c6a83bfd6796b825c493
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-04-19 19:34:51 +0100

    update

--- app/ui/productCard.tsx
+++ app/ui/productCard.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const ProductCard = () => {
   return (
-    <div className="relative p-4 rounded-md shadow-sm w-fit bg-white flex flex-col justify-center items-center">
-      <div className="w-full py-5 mb-1 flex items-center justify-center">
+    <div className="relative w-fit flex flex-col justify-center items-center">
+      <div className="w-full rounded-lg py-5 mb-1 bg-[#f4f4f4] flex items-center justify-center">
         <Image
           src="/ts2.jpeg"
           width={100}
@@ -15,9 +15,9 @@ const ProductCard = () => {
       <div>
         <p className="font-semibold">Tecno Camon 20+</p>
         <p className="text-sm text-gray-600">sharp camera, fast, 5G network</p>
-        <p className="font-semibold text-lg text-orange mb-2">$200</p>
+        <p className="font-semibold text-lg mb-2">$200</p>
       </div>
-      <button className="border rounded-full border-solid border-gray-200 w-full p-1 text-sm font-semibold">
+      <button className="rounded-full bg-orange w-full p-2 text-sm text-white font-semibold">
         Add To Cart
       </button>
     </div>
