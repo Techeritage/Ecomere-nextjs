2b562cd82157558d9d43451f748b521b6376ca55
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-04-20 10:06:01 +0100

    update

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -3,40 +3,49 @@ import {
   TruckIcon,
   WalletIcon,
 } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div className="flex flex-wrap px-[10%] justify-between mb-10">
+    <div className="grid grid-cols-2 gap-x-2 gap-y-3 py-4 px-[5%] md:px-[10%] justify-between mb-10">
       <div className="flex items-center gap-2">
-        <WalletIcon className="text-gray-800 stroke-1" width={50} />
+        <WalletIcon className="text-gray-800 stroke-1" width={30} />
         <div>
-          <p className="font-semibold">90 DAYS RETURN</p>
-          <p className="text-slate-500">If goods have problems</p>
+          <p className="font-semibold text-sm m-0">90 DAYS RETURN</p>
+          <p className="text-slate-500 text-[12px] m-0">
+            If goods have problems
+          </p>
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <GlobeAltIcon className="text-gray-800 stroke-1" width={50} />
+        <GlobeAltIcon className="text-gray-800 stroke-1" width={30} />
         <div>
-          <p className="font-semibold">24/7 SUPPORT</p>
-          <p className="text-slate-500">Anywhere in the world</p>
+          <p className="font-semibold text-sm m-0">24/7 SUPPORT</p>
+          <p className="text-slate-500 text-[12px] m-0">
+            Anywhere in the world
+          </p>
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <TruckIcon className="text-gray-800 stroke-1" width={50} />
+        <TruckIcon className="text-gray-800 stroke-1" width={30} />
         <div>
-          <p className="font-semibold">FREE DELIVERY</p>
-          <p className="text-slate-500">When ordering from $500</p>
+          <p className="font-semibold text-sm m-0">FREE DELIVERY</p>
+          <p className="text-slate-500 text-[12px] m-0">
+            When ordering from $500
+          </p>
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <img
+        <Image
+          width={30}
+          height={30}
           src="/credit-card.png"
           alt="credit card icon"
-          className="w-[50px]"
+          //className="w-[50px]"
         />
         <div>
-          <p className="font-semibold">SECURE PAYMENT</p>
-          <p className="text-slate-500">100% secure payment</p>
+          <p className="font-semibold text-sm m-0">SECURE PAYMENT</p>
+          <p className="text-slate-500 text-[12px] m-0">100% secure payment</p>
         </div>
       </div>
     </div>
