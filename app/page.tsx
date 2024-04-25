3f0f903941ce7a37463186aa74de0c04a3638f6b
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-04-25 21:43:45 +0100

    removed some folder to be able to avoid build error on  vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
 import Hero from "./ui/hero";
-import Logo from "./ui/logo";
-import Categories from "./ui/categories";
-import Homeproduct from "./ui/home-product";
-import Deals from "./ui/deals";
-import Test from "./ui/test";
-import { Suspense } from "react";
-import { CardsSkeleton } from "./ui/skeletons";
 
 export default function Home() {
   return (
@@ -13,11 +6,7 @@ export default function Home() {
       <section>
         <Hero />
       </section>
-      <section>
-        <Suspense fallback={<CardsSkeleton />}>
-          <Categories />
-        </Suspense>
-      </section>
+      
     </main>
   );
 }
