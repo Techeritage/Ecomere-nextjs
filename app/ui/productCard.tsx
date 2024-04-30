474b465fc02fc0ba3955d5aa1e5c2ad2c89d2c93
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-04-30 10:51:07 +0100

    update

--- app/ui/productCard.tsx
+++ app/ui/productCard.tsx
@@ -23,39 +23,30 @@ const ProductCard = ({
   btnBg: string;
 }) => {
   return (
-    <Link href={href} className="no-underline">
-      <div
-        className={`relative rounded-md max-w-[200px] h-[290px]`}
-        style={{ backgroundColor: `${bg}` }}
-      >
-        <div className="w-full rounded-lg h-[120px] mb-1 flex items-center justify-center">
-          <Image
-            src={images[0]}
-            width={75}
-            height={70}
-            alt={`${name} image`}
-          />
-        </div>
-        <div className="w-full p-2">
-          <RatingStar count={5} />
-          <p className="font-semibold text-sm mb-1 text-gray-700">{name}</p>
-          <p className="text-[12px] max-h-[40px] truncate ... mb-1 text-gray-600 md:text-sm">
-            {desc}
-          </p>
-          <p
-            className={`${ptSans.className} text-black font-bold text-lg mb-2`}
-          >
-            ${price}
-          </p>
-        </div>
-        <button
-          className={`text-black absolute bottom-2 left-[50%] translate-x-[-50%] rounded-full w-[90%] p-2 text-sm font-semibold`}
-          style={{ backgroundColor: `${btnBg}` }}
-        >
-          Add To Cart
-        </button>
+    <div
+      className={`relative rounded-md max-w-[200px] h-[290px]`}
+      style={{ backgroundColor: `${bg}` }}
+    >
+      <div className="w-full rounded-lg h-[120px] mb-1 flex items-center justify-center">
+        <Image src={images[0]} width={75} height={70} alt={`${name} image`} />
       </div>
-    </Link>
+      <div className="w-full p-2">
+        <RatingStar count={5} />
+        <p className="font-semibold text-sm mb-1 text-gray-700">{name}</p>
+        <p className="text-[12px] max-h-[40px] truncate ... mb-1 text-gray-600 md:text-sm">
+          {desc}
+        </p>
+        <p className={`${ptSans.className} text-black font-bold text-lg mb-2`}>
+          ${price}
+        </p>
+      </div>
+      <button
+        className={`text-black absolute bottom-2 left-[50%] translate-x-[-50%] rounded-full w-[90%] p-2 text-sm font-semibold`}
+        style={{ backgroundColor: `${btnBg}` }}
+      >
+        Add To Cart
+      </button>
+    </div>
   );
 };
 
