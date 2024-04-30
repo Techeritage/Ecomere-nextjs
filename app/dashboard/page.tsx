ae8ce0f978c9ca47eee205b39b7bc2438cd9f2f4
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-04-30 12:15:42 +0100

    test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,47 @@
+"use client";
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../lib/handleForm";
 import Hero from "../ui/hero";
 import Homeproduct from "../ui/home-product";
 import Logo from "../ui/logo";
+import { ProductData } from "../lib/definitions";
 
 export default function Dashboardpage() {
+  const [products, setProducts] = useState<ProductData[]>([]);
+
+  const fetchData = async () => {
+    const res = await fetchProducts();
+    if (res?.data) {
+      setProducts(res.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <main>
-      <section>
+      {products.length > 0 ? (
+        products.map((p) => (
+          <p>{p.name}</p>
+        ))
+      ) : (
+        <p>no product found</p>
+      )}
+      {/*<section>
         <Hero />
       </section>
-      {/*<section>
+      <section>
         <Suspense fallback={<CardsSkeleton />}>
           <Categories />
         </Suspense>
-  </section> */}
+  </section> 
       <section>
         <Homeproduct />
       </section>
       <section>
         <Logo />
-      </section>
+      </section> */}
     </main>
   );
 }
